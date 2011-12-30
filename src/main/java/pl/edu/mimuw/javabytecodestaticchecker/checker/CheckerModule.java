4f424bb38f2c6a037f037f34f61b8e6ab5ab22c0
Michal Ziemba <ziembal86@gmail.com>
2011-12-30 12:52:24 +0100

    added grammar file; pom.xml refactoring

---
+++ src/main/java/pl/edu/mimuw/javabytecodestaticchecker/checker/CheckerModule.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.mimuw.javabytecodestaticchecker.checker;
+
+/**
+ *
+ * @author michal
+ */
+public class CheckerModule {
+    
+}
